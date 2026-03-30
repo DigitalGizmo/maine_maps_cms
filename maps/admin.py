a63058d106514a442a0c02a538a3c1bd21aa1ca3
Don Button <37458717+DigitalGizmo@users.noreply.github.com>
2026-03-30 17:14:00 -0400

    add new fields to admin

--- maps/admin.py
+++ maps/admin.py
@@ -5,9 +5,10 @@ from .models import MapSet, MapView
 
 @admin.register(MapSet)
 class MapSetAdmin(admin.ModelAdmin):
-    list_display = ['title', 'slug', 'map_orientation', 'aspect_ratio']
+    list_display = ['title', 'slug','map_orientation', 'aspect_ratio']
     prepopulated_fields = {'slug': ('title',)}
-    fields = ['title', 'slug', 'short_description', 'map_orientation', 'aspect_ratio']
+    fields = ['title', 'short_title', 'slug', 'date', 'short_description', 
+              'map_orientation', 'aspect_ratio']
 
 
 @admin.register(MapView)
@@ -16,7 +17,7 @@ class MapViewAdmin(SummernoteModelAdmin):
     list_display = ['caption', 'mapset', 'ordinal', 'filename', 'is_crop']
     list_filter = ['mapset']
     fields = [
-        'mapset', 'ordinal', 'filename', 'caption', 'interpretive_text',
+        'mapset', 'ordinal', 'filename', 'caption', 'title', 'interpretive_text',
         'is_crop', 'crop_x', 'crop_y', 'crop_width', 'crop_height',
         'image_width', 'image_height',
     ]
